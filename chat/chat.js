7bda53f6adb36fdc0570a579109e8f60b44ca9a7
wissar <masterk850@gmail.com>
2016-01-18 10:43:49 -0600

    chat

---
+++ chat/chat.js
@@ -0,0 +1,23 @@
+angular.module('myApp', ['lumx', 'firebase'])
+    .controller('ctrlChat', function($scope, $firebaseArray, LxNotificationService) {
+        var ref = new Firebase('https://wissar-chat.firebaseio.com');
+        $scope.messages = $firebaseArray(ref);
+        console.log($scope.messages);
+        $scope.sendMessage = function() {
+            if ($scope.user.message) {
+            	var user = $scope.user.name || 'Piter la anguila';
+                $scope.messages.$add({
+                    from: user,
+                    body: $scope.user.message
+                });
+            }
+            $scope.user.message = '';
+        }
+
+        $scope.messages.$watch(function(data){
+        	console.log(data);
+        	var message = $scope.messages.$getRecord(data.key);
+        	console.log(message.from);
+        	LxNotificationService.notify('Mensaje nuevo de: ' + message.from + 'message-processing', false);
+        })
+    })
